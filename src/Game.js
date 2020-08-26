f978fd9115f8dfb1889d3e21c8e3c29474be1e7f
Uzias Rivera <uziasrivera@gmail.com>
2020-08-26 00:19:19 -0700

    added jump logic

--- src/Game.js
+++ src/Game.js
@@ -7,18 +7,17 @@ const Game = () => {
         [1, 0, 1, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 1],
         [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
         [2, 0, 2, 0, 2, 0, 2, 0],
         [0, 2, 0, 2, 0, 2, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0]
     ])
 
-    const [focusedPiece, setFocusedPieced] = useState()
-    const [focusedPieceTravels, setFocusedPiecedTravels] = useState()
+    const [focusedPiece, setFocusedPiece] = useState([])
+    const [focusedNum, setFocusedNum] = useState()
+    const [focusedPieceTravels, setFocusedPiecedTravels] = useState({})
     const movementLogic = (num, coordinate, initialBoard = board, ) => {
-        //[1,2] --> [0,1], [0,3], [2,1],[2,3]
         const radius = []
-        console.log(num)
         if (num === 2) {
             console.log("hello")
             radius.push([coordinate[0] - 1, coordinate[1] - 1])
@@ -28,20 +27,12 @@ const Game = () => {
             radius.push([coordinate[0] + 1, coordinate[1] + 1])
         }
 
-        const potentialSpaces = radius.filter(space => (initialBoard[space[0]] != undefined && initialBoard[space[0]][space[1]] != undefined))
+        const potentialSpaces = radius.filter(space => (initialBoard[space[0]] !== undefined && initialBoard[space[0]][space[1]] !== undefined))
 
         const possibleSpaces = potentialSpaces.map(space => {
             if (initialBoard[space[0]][space[1]] == 0) {
                 return space
-            } else if (initialBoard[space[0]][space[1]] != num) {
-                // [4,5] [3,6]  4 - 3 = 1 5 -6 = -1    [-1,  1] [3,4] [4,5] [-1, 1]
-                // 3-4, 6-5 = [-1, 1]                           3 - 4, 4 - 5
-                // y1 - y0, x1 - x0                             4 - 3, 5 - 4                                             
-                // 
-
-
-
-                // coordinate - enemyCoordinate = direction
+            } else if (initialBoard[space[0]][space[1]] !== num) {
                 const direction = [space[0] - coordinate[0], space[1] - coordinate[1]]
                 const jumpSpot = [space[0] + direction[0], space[1] + direction[1]]
                 if (initialBoard[jumpSpot[0]] && initialBoard[jumpSpot[0]][jumpSpot[1]] == 0) {
@@ -49,16 +40,67 @@ const Game = () => {
                 }
             }
         })
-        console.log(possibleSpaces)
-        return possibleSpaces
+        return possibleSpaces.filter(space => space)
     }
+
+    const onPieceClick = (col, rowIndex, colIndex) => {
+        setFocusedPiecedTravels({})
+        setFocusedPiece([rowIndex, colIndex])
+        setFocusedNum(col)
+        const movements = movementLogic(col, [rowIndex, colIndex])
+        setFocusedPiecedTravels(() => {
+            const paths = {}
+            movements.forEach(travels => {
+                if (travels !== undefined) {
+                    const key = travels[0]
+                    const value = travels[1]
+                    if (key in paths) {
+                        paths[key] = [...paths[key], value]
+                    } else {
+                        paths[key] = [value]
+                    }
+                }
+            })
+            return paths
+        })
+    }
+
+    const makeMove = (col, rowIndex, colIndex) => {
+        if (Math.abs(focusedPiece[0] - rowIndex)==2) {
+            setBoard(()=>{
+                const newBoard = [...board]
+                const diffenceRate = [(rowIndex - focusedPiece[0])/2, (colIndex - focusedPiece[1])/2]
+                console.log(diffenceRate)
+                newBoard[focusedPiece[0] + diffenceRate[0]][ focusedPiece[1] + diffenceRate[1]] = 0
+                newBoard[rowIndex][colIndex] = focusedNum
+                newBoard[focusedPiece[0]][focusedPiece[1]] = col
+                return newBoard
+            })
+
+        } else {
+            setBoard(() => {
+                const newBoard = [...board]
+                newBoard[rowIndex][colIndex] = focusedNum
+                newBoard[focusedPiece[0]][focusedPiece[1]] = col
+                return newBoard
+            })
+        }
+        setFocusedPiece([])
+        setFocusedPiecedTravels({})
+        setFocusedNum()
+
+    }
+
     return (
         <div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
             {board.map((row, rowIndex) => (
                 <div style={{ display: "flex", textAlign: "center", alignSelf: "center" }}>
-                    {row.map((col, colIndex) => (
-                        <p onClick={(e) => col != 0 ? movementLogic(col, [rowIndex, colIndex]) : null} style={{ color: col == 0 ? "black" : col == 1 ? "blue" : "red" }}>{col.toString()}</p>
-                    ))}
+                    {row.map((col, colIndex) => {
+                        return <p
+                            className={`piece-${col} piece`}
+                            style={rowIndex in focusedPieceTravels && focusedPieceTravels[rowIndex].includes(colIndex) ? { color: "dodgerblue", cursor: "pointer" } : {}}
+                            onClick={(e) => col == 1 || col == 2 ? onPieceClick(col, rowIndex, colIndex) : rowIndex in focusedPieceTravels && focusedPieceTravels[rowIndex].includes(colIndex) ? makeMove(col, rowIndex, colIndex) : null}>{col.toString()}</p>
+                    })}
                 </div>
             ))}
         </div>
